23379858af1a51e17eb29433f4c39f7a77df0a09
Candice <chienting.huang@gmail.com>
2017-07-19 20:47:22 +0800

    my first commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Candice on 2017/7/19.
+//  Copyright © 2017年 appletea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theLabel: UILabel!
+    
+
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBAction func button(_ sender: Any) {
+        print(text1.text!)
+        print(text2.text!)
+        
+    }
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
